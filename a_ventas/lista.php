b50aab06a9a96015b4b7aaa90ce9c91c3e2fddb8
ericesponda <>
2020-12-13 20:57:21 -0600

    x
    x

--- a_ventas/lista.php
+++ a_ventas/lista.php
@@ -24,19 +24,19 @@
 			</div>
 		</div>
 		<div class='row header-row'>
-			<div class='col-12 col-sm-12 col-md-2 col-lg-2 col-xl-2'>#</div>
-			<div class='col-12 col-sm-12 col-md-2 col-lg-2 col-xl-2'>Numero</div>
-			<div class='col-12 col-sm-12 col-md-2 col-lg-2 col-xl-2'>Comanda</div>
-			<div class='col-12 col-sm-12 col-md-2 col-lg-2 col-xl-2'>Fecha</div>
-			<div class='col-12 col-sm-12 col-md-2 col-lg-2 col-xl-2'>Cliente</div>
-			<div class='col-12 col-sm-12 col-md-2 col-lg-2 col-xl-2'>Total</div>
+			<div class='col-12 col-xl col-auto'>#</div>
+			<div class='col-12 col-xl col-auto'>Numero</div>
+			<div class='col-12 col-xl col-auto'>Comanda</div>
+			<div class='col-12 col-xl col-auto'>Fecha</div>
+			<div class='col-12 col-xl col-auto'>Cliente</div>
+			<div class='col-12 col-xl col-auto'>Total</div>
 		</div>
 
 			<?php
 				foreach($pd as $key){
 			?>
 					<div class='row body-row' draggable='true'>
-						<div class='col-12 col-sm-12 col-md-2 col-lg-2 col-xl-2' >
+						<div class='col-12 col-xl col-auto' >
 							<div class="btn-group">
 								<?php
 									if($db->nivel_captura==1){
@@ -45,11 +45,11 @@
 								?>
 							</div>
 						</div>
-						<div class='col-12 col-sm-12 col-md-2 col-lg-2 col-xl-2 text-center' data-titulo='Numero'><?php echo $key->numero; ?></div>
-						<div class='col-12 col-sm-12 col-md-2 col-lg-2 col-xl-2 text-center' data-titulo='Comanda'><?php echo $key->comanda; ?></div>
-						<div class='col-12 col-sm-12 col-md-2 col-lg-2 col-xl-2' data-titulo='Fecha'><?php echo fecha($key->fecha,2); ?></div>
-						<div class='col-12 col-sm-12 col-md-2 col-lg-2 col-xl-2' data-titulo='Cliente'><?php echo $key->nombre; ?></div>
-						<div class='col-12 col-sm-12 col-md-2 col-lg-2 col-xl-2' align="center" data-titulo='Total'><?php echo moneda($key->total); ?></div>
+						<div class='col-12 col-xl col-auto text-center' data-titulo='Numero'><?php echo $key->numero; ?></div>
+						<div class='col-12 col-xl col-auto text-center' data-titulo='Comanda'><?php echo $key->comanda; ?></div>
+						<div class='col-12 col-xl col-auto' data-titulo='Fecha'><?php echo fecha($key->fecha,2); ?></div>
+						<div class='col-12 col-xl col-auto' data-titulo='Cliente'><?php echo $key->nombre; ?></div>
+						<div class='col-12 col-xl col-auto' align="center" data-titulo='Total'><?php echo moneda($key->total); ?></div>
 
 
 					</div>
